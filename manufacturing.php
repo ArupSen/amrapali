e1880ef3c17dec2d57c52e09c2f324d36f0723ea
Arup Sen <arupsen48@gmail.com>
2014-03-07 01:47:46 +0000

    updated banner image

---
+++ manufacturing.php
@@ -0,0 +1,27 @@
+<?php
+// -- manufacturing.php general --
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+<!-- <div class="logo"></div> -->
+<?php img_tag('amrapali_design_studio.jpg','','collections-logo'); ?>
+<!-- <div class="sub-nav">
+	<a href="" onclick="return false;">FACTORY</a>
+	<a href="http://martnerandmowat.com/amrapali/contact.php">GENERAL</a>
+	<a href="http://martnerandmowat.com/amrapali/contact/careers.php">CAREERS</a>
+</div> -->
+
+<div class="contact_form"><h3>Amrapali Manufacturing Units</h3>
+<hr />
+<p class="contact_info">Amrapali Design Studio & Amrapali Exports cater to domestic and international markets respectively. Manufacturing jewellery for leading international stores and designers, the workshops have installed all productions in house as per international standards.<br /><br />
+Based in the city of Jaipur, Amrapali offers a wide range of bespoke jewellery as per buyers specification in precious and non-precious metals. With the capacity of more than a thousand workers, our manufacturing units are sophisticated with modern machinery, well-trained personnel, and absolute dedication to perfection.</p>
+</div>
+
+<?php 
+// filename, title, id
+// img_tag('','',''); ?>
+
+<?php footer(); ?>
