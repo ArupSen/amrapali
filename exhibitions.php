14abaff003caec7c3442726496ec493c7f95751f
Arup Sen <arupsen48@gmail.com>
2014-03-07 20:37:32 +0000

    added all the text marked up

---
+++ exhibitions.php
@@ -0,0 +1,68 @@
+<?php
+// -- exhibitions.php --
+
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+<?php img_tag('exhibitions.jpg','','spotlight-logo'); ?>
+<?php require_once INCPATH.'spotlight-nav.php'; ?>
+<?php img_tag('exhibitions.png','','exhibitions'); ?>
+<div class="exhibitions">
+<h3>HONG KONG</h3>
+  <div>
+    <hr />
+    <p>
+    Hong Kong International Jewellery Show 2014<br />
+    5th- 9th March, 2014<br /> 
+    Hong Kong Convention & Exhibition Centre<br />
+    Wan Chai<br />
+    Hong Kong<br />
+    5CON- 33/34/35    
+    </p>
+    <p>
+    Hong Kong Jewellery & Gem Fair 2014<br />
+    17th- 21st September, 2014<br /> 
+    Hong Kong Convention & Exhibition Centre<br />
+    Wan Chai<br />
+    Hong Kong<br />
+    5F- 105, 107, 109    
+    </p>
+  </div>
+<h3>USA</h3>
+  <div>
+    <hr />
+    <p>
+    Couture Las Vegas Show 2014<br />
+    May 28th- June 2, 2014<br /> 
+    Las Vegas - Wynn Hotel<br />
+    3131 Las Vegas Blvd. South<br />
+    Las Vegas<br />
+    USA
+    </p>
+  </div>
+<h3>UK</h3>
+  <div>
+    <hr />
+    <p>
+    Inhorgenta, Munich 2014<br />
+    14th - 17th February, 2014<br /> 
+    British Pavillion Hall C2<br />
+    </p>
+    <p>
+    Jewellery & Watch London, at the<br />
+    18th- 19th June, 2014<br /> 
+    Saachi Gallery London<br />
+    Stand 5G-02<br />
+    </p>
+    <p>
+    International Jewellery London Show 2014 (IJL)<br />
+    31st August-2nd September 2014<br /> 
+    A Olympia London<br />
+    stand D-101<br />
+    </p>
+  </div>
+</div>
+<?php footer(); ?>
