b73fbc1ad11545730e6d1cb5982244e9a9096aa7
Arup Sen <arupsen48@gmail.com>
2014-03-08 01:06:40 +0000

    added final text as markup in place of image text

--- index.php
+++ index.php
@@ -8,19 +8,27 @@ navbar();
 ?>
 <!-- Section #1 / Intro -->
 <section id="first" class="story first" data-speed="8" data-type="background">    	
-	<!-- <div class="smashinglogo" data-type="sprite" data-offsetY="100" data-Xposition="50%" data-speed="-2"></div> -->		
+	<div class="symbols" data-type="sprite" data-offsetY="600" data-speed="4"></div>		<!-- data-type="background" data-offsetY="650" data-Xposition="50%" data-speed="4" -->
 	<article>
-
-		<img src="images/eternal_txt.png" alt="tutorial-title" width="711" height="242" />
+		<div class="eternal"></div>
+		<!-- <img src="images/eternal_mystery_background.png" alt="tutorial-title" width="1920" height="1400" /> -->
 		
     </article>
+	
 </section>
 
 <!-- Section #2 / Background Only -->
 <section id="second" class="story first" data-speed="8" data-type="background">
 	<article>
 		<img src="images/muse_txt.png" alt="tutorial-title" width="782" height="364" />
-		
+  <div class="muse-text">
+<h3>THE WORLD OF AMRAPALI</h3>
+<hr />
+<h2>OUR MUSE</h2>
+<h3>THE SOUL OF THIS WORLD, IT'S DRIVING INSPIRATION</h3>
+<hr />
+<p>Amrapali was a renowned royal courtesan said to have lived around 500BC in ancient India, she is also the timeless symbol of a Muse, one who brings you inspiration, intuition and higher vision through her timeless beauty and grace. Amrapali's extraordinary beauty and charm was also known far and wide. She is mentioned in the old Pali and Buddhist traditions, particularly in conjunction with the Buddha staying at her mango grove. The timelessness of the legend, and its immediate association to feminine beauty, delight and India's heritage make it the ideal association for the brand.</p>
+  </div>  
 	</article>
 </section>
 
@@ -30,12 +38,21 @@ navbar();
 	<article>
 		<img src="images/heritage_txt.png" alt="tutorial-title" width="887" height="316" />
 	    
+  <div class="heritage-text">
+<h3>TREASURES FROM THE</h3>
+<hr />
+<h2>ROOTS</h2>
+<h3>Ancient Art revival</h3>
+<hr />
+<p>Led by the passion of its founders, Rajiv Arora & Rajesh Ajmera, two heritage enthusiasts and history students, Amrapali is the finest reflection of India's unmatched jewellery traditions and workmanship.</p>
+  </div>  
 	</article>
 </section>	
 
 
 <!-- Section #4 / HTML5 Video -->
-<section id="fourth" class="story first" data-speed="8" data-type="background">
+<section id="fourth" class="story first" data-speed="16" data-type="background" data-offsetY="200">
+	<div class="lens-flare" data-type="sprite" data-offsetY="1" data-speed="4"></div>
 	<article>
 		<img src="images/passion_txt.png" alt="tutorial-title" width="467" height="369" />
     
@@ -43,17 +60,20 @@ navbar();
 
 </section>	
 <!-- Section #1 / Intro -->
-<section id="fifth" class="story first .byebye" data-speed="8" data-type="background">    	
+<section id="fifth" class="story first .byebye" data-speed="8" data-type="background" data-offsetY="200">    	
 	<!-- <div class="smashinglogo" data-type="sprite" data-offsetY="100" data-Xposition="50%" data-speed="-2"></div> -->		
 	<article>
-
-		<!-- <img src="images/eternal_txt.png" alt="tutorial-title" width="711" height="242" /> -->
+  <div class="heritage-text">
+<h3>Extraordinary</h3>
+<hr />
+<h2>CRAFTSMANSHIP</h2>
+<h3>Support to craftsmen and the legacy of generations</h3>
+<hr />
+<p>Renowned for its signature magnificent pieces and rare craftsmanship, Amrapali harmonizes classical tradition, with modern interpretation of ancient aesthetic and techniques.</p>
+  </div>  
+		<img src="images/craftmanship_txt.png" alt="craftmanship-title" width="939" height="309" />
 		
     </article>
 </section>
-
-<?php 
-// filename, title, id
-// img_tag('','',''); ?>
 
 <?php footer(); ?>
