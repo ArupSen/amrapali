0d5cc0e51731126256fa01d4175a7f9f357f0efe
Arup Sen <arupsen48@gmail.com>
2014-03-08 01:12:56 +0000

    added show date & booth no.

--- exhibitions.php
+++ exhibitions.php
@@ -16,19 +16,19 @@ navbar();
     <hr />
     <p>
     Hong Kong International Jewellery Show 2014<br />
-    5th- 9th March, 2014<br /> 
+    Show date: 5th- 9th March, 2014<br /> 
     Hong Kong Convention & Exhibition Centre<br />
     Wan Chai<br />
     Hong Kong<br />
-    5CON- 33/34/35    
+    Booth No. 5CON- 33/34/35    
     </p>
     <p>
     Hong Kong Jewellery & Gem Fair 2014<br />
-    17th- 21st September, 2014<br /> 
+    Show date: 17th- 21st September, 2014<br /> 
     Hong Kong Convention & Exhibition Centre<br />
     Wan Chai<br />
     Hong Kong<br />
-    5F- 105, 107, 109    
+    Booth No. 5F- 105, 107, 109    
     </p>
   </div>
 <h3>USA</h3>
@@ -36,7 +36,7 @@ navbar();
     <hr />
     <p>
     Couture Las Vegas Show 2014<br />
-    May 28th- June 2, 2014<br /> 
+    Show date: May 28th- June 2, 2014<br /> 
     Las Vegas - Wynn Hotel<br />
     3131 Las Vegas Blvd. South<br />
     Las Vegas<br />
@@ -48,20 +48,20 @@ navbar();
     <hr />
     <p>
     Inhorgenta, Munich 2014<br />
-    14th - 17th February, 2014<br /> 
+    Show date: 14th - 17th February, 2014<br /> 
     British Pavillion Hall C2<br />
     </p>
     <p>
     Jewellery & Watch London, at the<br />
-    18th- 19th June, 2014<br /> 
+    Show date: 18th- 19th June, 2014<br /> 
     Saachi Gallery London<br />
-    Stand 5G-02<br />
+    Booth No. Stand 5G-02<br />
     </p>
     <p>
     International Jewellery London Show 2014 (IJL)<br />
-    31st August-2nd September 2014<br /> 
+    Show date: 31st August-2nd September 2014<br /> 
     A Olympia London<br />
-    stand D-101<br />
+    Booth No. stand D-101<br />
     </p>
   </div>
 </div>
