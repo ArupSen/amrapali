086203e3c0743eef2131e65faabf3d5913a3f900
Arup Sen <arupsen48@gmail.com>
2014-03-12 23:57:28 +0000

    first commit

---
+++ story.php
@@ -0,0 +1,16 @@
+<?php
+// -- story.php --
+
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+<div id="story">
+  <iframe width="853" height="480" src="//www.youtube.com/embed/SvfYk_5sU3A?rel=0" frameborder="0" allowfullscreen></iframe>
+</div>
+
+
+
+<?php footer(); ?>
