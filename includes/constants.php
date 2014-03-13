f61df6cd8c0d37c2992351f38685305018f11a56
Arup Sen <arupsen48@gmail.com>
2014-03-13 00:16:57 +0000

    added tile_path_prefix

--- includes/constants.php
+++ includes/constants.php
@@ -5,6 +5,7 @@ define('IMGPATH', './images/');
 define('DATAPATH', './data/');
 define('CSSPATH', './css/');
 define('JSPATH', './js/');
+define('TILE_PATH_PREFIX','http://yogarup.com/amrapali/images/collections'); // no trailing slash
 
 
 ?>
