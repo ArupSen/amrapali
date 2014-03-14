a976c32106ca2e2552c32a1abac946b7488b39e6
Arup Sen <arupsen48@gmail.com>
2014-03-14 15:24:30 +0000

    added 2 new bangalore addresses

--- stores.php
+++ stores.php
@@ -4,10 +4,320 @@
 require_once 'inc_path.php';
 require_once INCPATH.'header.php';
 navbar();
-
+function map_link($value='')
+{
+	echo '<a href="$value" target="_blank">see map</a>';
+}
 // the unique contents will go here up to the footer
 ?>
+<?php img_tag('Stores.jpg', '', 'stores-logo'); ?>
 
 
+<?php //img_tag('stores-india.png', '', 'stores-india'); ?>
+<div id="store_locations">
+<h3>JAIPUR</h3>
+<div>
+	<hr />
+	<p>
+	Amrapali<br />
+	Panch Batti, M.I.Road,<br /> 
+	Jaipur 302001<br />
+	Tel.: +91-141-5118215/16 <br />
+	<a href="mailto:pb@amrapalijewels.com ">pb@amrapalijewels.com </a><br /><br />
+	<a href="https://maps.google.co.uk/maps?q=Amrapali+Jewels&hl=en&cid=4428115416878856604&gl=GB&t=m&z=16&iwloc=A" target="_blank">see map</a>
+	</p>
+<p>
+	Amrapali<br />
+	132, Hotel Rambagh Palace, Jaipur 302001<br />
+	Tel: +91-141-5103156<br />
+	<a href="mailto:amrapali@amrapalijewels.com">amrapali@amrapalijewels.com</a><br /><br />
+<a href="https://maps.google.com/maps?q=132,+Hotel+Rambagh+Palace,+Jaipur+302001&hl=en&ll=26.916254,75.810127&spn=0.137912,0.204449&sll=26.898037,75.808067&sspn=0.137934,0.204449&hq=132,+Hotel+Rambagh+Palace,+Jaipur+302001&radius=15000&t=m&z=13" target="_blank">see map</a>  </p>
+<p>
+	Amrapali<br />
+	Palace Atelier<br />
+	M.S.M.S. II, Museum The City Palace<br />
+	Jaipur 302002<br /><br />
+<a href="https://maps.google.com/maps?q=Palace+Atelier,+S.M.S.+II,+Museum+The+City+Palace,+Jaipur+302002&hl=en&sll=26.887743,75.814522&sspn=0.137946,0.204449&hq=palace+atelier+sms+ii+museum+the+city+palace&hnear=302002,+India&t=m&z=14" target="_blank">see map</a> </p>
+<p>
+	Amrapali Corporate Office<br />
+	K-14/B, Ashok Marg<br />
+	C-Scheme<br />
+	Jaipur 302001<br />
+	Tel: +91-141-5191100<br />
+	<a href="mailto:amrapali@amrapalijewels.com">amrapali@amrapalijewels.com</a><br /><br />
+	<a href="https://maps.google.com/maps?q=K-14%2FB,+Ashok+Marg,+jaipur+302001&hl=en&sll=26.925162,75.79082&sspn=0.00948,0.010128&hnear=Ashok+Marg,+Ashok+Nagar,+Jaipur,+Rajasthan+302001,+India&t=m&z=16" target="_blank">see map</a>
+	
+</p>
+</div>
+<h3>MUMBAI</h3>
+<div class="mumbai">
+		<hr />
+	<p>
+		Amrapali<br />
+		Juhu Church Road, Mumbai 400049<br />
+		Tel.: + 91 22 26125001/5002<br />
+		<a href="mailto:mumbaijuhu@amrapalijewels.com">mumbaijuhu@amrapalijewels.com</a><br /><br />
+
+	<a href="https://maps.google.com/maps?q=Phoolwari+Cottage,+Near+Prithvi+Theatre,+Mumbai+400049&hl=en&ll=19.103973,72.828627&spn=0.073074,0.102224&sll=18.927128,72.820907&sspn=0.002286,0.003195&hq=Phoolwari+Cottage,&hnear=Prithvi+Theatre,+India&t=m&z=14" target="_blank">see map</a> </p>
+		<p>
+		Amrapali Fine Jewels<br />
+		Shop No. 28 & 29, Oberoi Shopping Centre<br />
+		The Oberoi Hotel, Nariman Point<br />
+		Mumbai 400021<br />
+		Tel.: +91-22-22843687/8<br />
+		<a href="mailto:mumbaioberoi@amrapalijewels.com">mumbaioberoi@amrapalijewels.com</a><br /><br />
+		<a href="https://maps.google.com/maps?q=Oberoi+Shopping+Centre,+Nariman+Point,+Mumbai,+Maharashtra,+India&hl=en&ll=18.927128,72.820907&spn=0.002286,0.003195&sll=26.924646,75.793634&sspn=0.068951,0.102224&oq=Oberoi+Shopping+Centre&hq=Oberoi+Shopping+Centre,+Nariman+Point,+Mumbai,+Maharashtra,+India&t=m&z=19&iwloc=A" target="_blank">see map</a> </p>
+
+	<p>
+		Amrapali Silver<br />
+		Shop no. 62, Oberoi Shopping Center,<br />
+		The Oberoi Hotel, Nariman Point,<br /> 
+		Mumbai 400021<br />
+		Tel.: +91-22-22843687<br/>
+		<a href="mailto:mumbaioberoi@amrapalijewels.com">mumbaioberoi@amrapalijewels.com</a><br /><br />
+	<a href="https://maps.google.com/maps?q=Oberoi+Shopping+Centre,+Nariman+Point,+Mumbai,+Maharashtra,+India&hl=en&ll=18.927128,72.820907&spn=0.002286,0.003195&sll=26.924646,75.793634&sspn=0.068951,0.102224&oq=Oberoi+Shopping+Centre&hq=Oberoi+Shopping+Centre,+Nariman+Point,+Mumbai,+Maharashtra,+India&t=m&z=19&iwloc=A" target="_blank">see map</a> </p>
+	<p>
+		Amrapali<br />
+		at SR Artefacts<br />
+		Shop No.103, Next To RBI<br />
+		Dr. Anne Besant Road, Worli,<br /> 
+		Mumbai<br />
+		Tel.: +91 22 24407070<br /><br />
+	<a href="https://maps.google.com/maps?q=SR+Artefacts,+Dr.+Anne+Besant+Road,+Worli,+Mumbai&hl=en&sll=19.002745,72.817662&sspn=0.01828,0.025556&hq=SR+Artefacts,+Dr.+Anne+Besant+Road,+Worli,+Mumbai&radius=15000&t=m&z=14" target="_blank">see map</a> </p>
+
+<p>
+	Amrapali<br />
+	at Bombay Electric<br />
+	1 Reay House, BEST Marg,<br /> 
+	Mumbai 400001<br />
+	Tel: +91-22-22876296<br /><br />
+	<a href="https://maps.google.com/maps?q=1+Reay+House,+BEST+Marg&hl=en&sll=26.24871,73.049729&sspn=0.009536,0.010128&hq=1+Reay+House,+BEST+Marg&radius=15000&t=m&z=14" target="_blank">see map</a>	
+</p>
+<p>
+	Amrapali<br />
+	at Good Earth- Raghuvanshi Mills<br />
+	11-12 Raghuvanshi Mills Compound<br />
+	Senapati Bapat Marg, Lower Parel<br />
+	Mumbai 400013<br />
+	Tel: +91-22-24951954<br /><br />
+	<a href="https://maps.google.com/maps?q=11-12+Raghuvanshi+Mills+Compound&hl=en&sll=18.92114,72.832567&sspn=0.080462,0.081024&hnear=Raghuvanshi+Mills,+Senapati+Bapat+Marg,+Gandhi+Nagar,+Upper+Worli,+Lower+Parel,+Mumbai,+Maharashtra+400013,+India&t=m&z=17" target="_blank">see map</a>	
+</p>
+<p>
+	Amrapali<br />
+	at Good Earth- Colaba<br />
+	2 Reay House,<br />
+	Ormistone Road, Colaba<br />
+	Mumbai 400001<br />
+	Tel: +91-22-22021030<br /><br />
+	<a href="https://maps.google.com/maps?q=2+Reay+House,+Ormiston+Road,+Colaba&hl=en&sll=18.921805,72.831883&sspn=0.005029,0.005064&hq=2+Reay+House,+Ormiston+Road,+Colaba&t=m&z=17" target="_blank">see map</a>
+	
+</p>
+<p>
+	Amrapali<br />
+	at Chatrapati Shivaji International Airport,<br /> 
+	New Terminal 2, Unit No. W5 - 4109, Level 4 SHA,<br /> 
+	Sahar Road, Andheri (E)<br />
+	Mumbai 400099 <br />
+	Tel: +91-22-64519555<br /><br />
+	<a href="https://maps.google.com/maps?q=at+Chatrapati+Shivaji+International+Airport&hl=en&sll=18.921158,72.832507&sspn=0.010058,0.010128&t=m&z=17&iwloc=A" target="_blank">see map</a>
+	
+</p>
+</div>
+<h3>NEW DELHI</h3>
+<div>
+	<hr />
+<p>
+  	Amrapali<br />
+  	39/39A, Khan Market, New Delhi 110003<br />
+	Tel.: +91-11-41752024 / 41752025<br />
+	<a href="mailto:delhi@amrapalijewels.com">delhi@amrapalijewels.com</a><br /><br />
+<a href="https://maps.google.com/maps?q=Khan+Market,+New+Delhi+110003&hl=en&sll=18.995873,72.816159&sspn=0.073122,0.102224&t=m&z=17&iwloc=A" target="_blank">see map</a> </p>	
+<p>
+  	Amrapali<br />
+  	41, Santushti Shopping Complex<br />
+	Chanakya Puri, New Delhi<br />
+	Tel.: +91-11-24672025<br /><br />
+<a href="https://maps.google.com/maps?q=Santushti+Shopping+Complex,+Chanakyapuri,+New+Delhi,+Delhi,+India&hl=en&sll=37.0625,-95.677068&sspn=61.582079,104.677734&oq=Santushti+Shopping+Complex,+Chanakya+Puri,+New+Delhi&hq=Santushti+Shopping+Complex,&hnear=Chanakyapuri,+New+Delhi,+Delhi,+India&t=m&z=16" target="_blank">see map</a> </p>
+<p>
+	Amrapali 
+
+	at Good Earth<br />
+	Shop No.9 A.B.C<br />
+	Ground 1st & 2nd Floor,<br />
+	Khan Market,<br />
+	Delhi 110003<br />
+	Tel: +91-11-24647175<br /><br />
+	<?php map_link('https://maps.google.com/maps?q=Khan+Market,%09Delhi+110003&hl=en&sll=26.913875,75.808361&sspn=0.018961,0.020256&t=m&z=17&iwloc=A'); ?>
+	
+</p>
+<p>
+  	Amrapali<br />
+  	at HHEC'S National Museum Shop<br />
+	National Museum<br />
+	Janpath, New Delhi<br />
+	Tel.: +91-11-23062687<br /><br />
+<a href="https://maps.google.com/maps?q=National+Museum,+Janpath,+Rajpath+Road+Area,+Central+Secretariat,+New+Delhi,+Delhi,+India&hl=en&sll=28.595134,77.19808&sspn=0.016975,0.025556&oq=National+M,+New+Delhi,+Delhi,+India&hq=National+Museum,&hnear=Janpath,+Rajpath+Road+Area,+Central+Secretariat,+New+Delhi,+India&t=m&z=15" target="_blank">see map</a> </p>
+</div>
+<h3>BANGALORE</h3>
+
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+	Amrapali Fine Jewels<br />
+	No 10/4 ,Mithra Towers<br />
+	Kasturba Gandhi Road<br />
+	Bengaluru-1<br />
+	Tel: +91 80 22221622<br />
+	<a href="mailto:Bangalore@amrapalijewels.com">Bangalore@amrapalijewels.com</a><br />
+<a href="https://maps.google.com/maps?q=Mithra+Towers,+Kasturba+Gandhi+Road,+bengaluru&hl=en&ll=12.976059,77.599043&spn=0.00942,0.012778&sll=12.976653,77.5992&sspn=0.00942,0.012778&hnear=Mitra+Towers,+Kasturba+Rd,+Shanthala+Nagar,+Ashok+Nagar,+Bangalore,+Bangalore+Urban,+Karnataka+560001,+India&t=m&z=17" target="_blank">see map</a> </p>
+
+<p>
+	Amrapali<br />
+	at Raintree<br />
+	No.4, Sankey Road<br />
+	High Ground (Opp. Hotel Windsor Sheraton)<br />
+	Bengaluru - 560052<br />
+	Tel.: +91 80 32723251/22354396<br /><br />
+<a href="https://maps.google.com/maps?q=No.4,+Sankey+Road,+High+Ground,+Bengaluru+-+560052&hl=en&ll=12.994261,77.584645&spn=0.009419,0.012778&sll=13.003098,77.583675&sspn=0.037675,0.051112&hq=No.4,+Sankey+Road,+High+Ground,+Bengaluru+-+560052&radius=15000&t=m&z=17" target="_blank">see map</a> </p>
+<p>
+	Amrapali<br />
+	at Thar Handicrafts Gallery<br />
+	No. 534 (Opp. Dell)<br />
+	Kormangala Indra Nagar<br />
+	Intermediate Ring Road<br />
+	Amar Jyoti Layout, Domlur<br />
+	Bengaluru 560071<br />
+	Tel.: +91-80-25358717/25358747<br /><br />
+<a href="https://maps.google.com/maps?q=Thar+Handicrafts+Gallery,+Amarjyoti+Layout,+Domlur,+Bengaluru,+Karnataka,+India&hl=en&ll=12.953465,77.640681&spn=0.009421,0.012778&sll=12.955718,77.642516&sspn=0.009421,0.012778&t=m&z=17&iwloc=A" target="_blank">see map</a> </p>
+<p>
+Amrapali<br />
+At Kamya Hastkala Arts & Crafts Pvt. Ltd.<br />
+No. Sai Krupa, #1152, 12th Main, Hal 2nd stage,<br />
+Indiranagar, Bangalore 560008<br />
+Tel: +91 9880598731<br /><br />
+<a href="https://www.google.co.uk/maps/place/Kent+Projects,+Luxury+Villas+Bangalore,+North+Bangalore,+Developers%E2%80%8E/@12.9694366,77.6440144,18z/data=!4m2!3m1!1s0x0:0x9bcc8a121850531b" target="_blank">see map</a>
+</p>
+<p>
+Amrapali<br />
+At Shop Ananya<br />
+No. 24/1, Lavelle Road<br />
+Bangalore 560001<br />
+Tel: +91 80 22125208<br /><br />
+<a href="https://www.google.co.uk/maps/place/Lavelle+Rd/@12.9709041,77.5981658,19z/data=!4m2!3m1!1s0x3bae1679b09041a5:0x413706ed450cc2e1" target="_blank">see map</a>
+</p>
+
+</div>
+<h3>JODHPUR</h3>
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+	Ajit Bhawan, Shop no.4, Rani Bagh,<br />
+	Circuit House Road,<br />
+	Jodhpur 342001<br />
+	Tel: +91-291-5107373<br />
+	<a href="mailto:jodhpur@amrapalijewels.com ">jodhpur@amrapalijewels.com </a><br /><br />
+	<a href="https://maps.google.com/maps?q=Ajit+Bhawan,+Rani+Bagh,+Circuit+House+Rd,+Ajit+Colony,+Jodhpur,+Rajasthan,+India&hl=en&sll=26.24871,73.049729&sspn=0.138718,0.204449&hq=Ajit+Bhawan,+Rani+Bagh,&hnear=Circuit+House+Rd,+Ajit+Colony,+Jodhpur,+Rajasthan+342011,+India&t=m&z=17" target="_blank">see map</a>
+	</p>
+</div>
+<h3>CHENNAI</h3>
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+	Amethyst<br />
+	Whites Road, Royapettah,<br />
+	Chennai 600014<br />
+	Tel: +91-44-45991600/01<br/><br />
+
+<a href="https://maps.google.com/maps?q=Whites+Road,+Peters+Colony,+Royapettah,+Chennai,+Tamil+Nadu+600014,+India&hl=en&sll=26.282222,73.038085&sspn=0.008667,0.012778&oq=Whites+Road,+Royapettah,%09Chennai+600014&hnear=Whites+Rd,+Peters+Colony,+Royapettah,+Chennai,+Tamil+Nadu+600014,+India&t=m&z=17" target="_blank">see map</a> </p>
+</div>
+<h3>NEEMRANA</h3>
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+	at Neemrana Shop<br />
+	Neemrana Fort Palace,<br />
+	Village: Neemrana, Distt. Alwar<br />
+	Rajasthan<br />
+	Tel.: +91-149-42460025<br /><br />
+<a href="https://maps.google.com/maps?q=Neemrana+Fort-Palace,+Neemrana+Fort+Road,+Neemrana,+Rajasthan,+India&hl=en&sll=13.056919,80.259158&sspn=0.009417,0.012778&oq=Neemrana+Fort+Palace&hq=Neemrana+Fort-Palace,+Neemrana+Fort+Road,+Neemrana,+Rajasthan,+India&t=m&z=17" target="_blank">see map</a> </p>
+</div>
+<h3>HYDERABAD</h3>
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+	at The Good Earth Company<br />
+	567A, Road 12 Banjara Hills,<br />
+	Hyderabad 500 029<br />
+	Tel: +91-40-23327000<br /><br />
+	
+<a href="https://maps.google.com/maps?q=Road+12+Banjara+Hills,Hyderabad+500+029&hl=en&sll=28.616025,77.227801&sspn=0.008486,0.012778&hnear=500,+Road+Number+12,+NBT+Nagar,+Banjara+Hills,+Hyderabad,+Andhra+Pradesh+500034,+India&t=m&z=17" target="_blank">see map</a> </p>
+<p>
+	Amrapali<br />
+	at Elahe<br />
+	Milestone 476<br />
+	Road No. 10, Banjara Hills<br />
+	Hyderabad 500034<br />
+	Tel.: +91 40 23350130<br /><br />
+
+	
+<a href="https://maps.google.com/maps?q=Elahe,+Road+No+10,+Zehra+Nagar,+Banjara+Hills,+Hyderabad,+Andhra+Pradesh+500034,+India&hl=en&sll=17.415015,78.444877&sspn=0.004612,0.006389&oq=elahRoad+10+Banjara+Hills,Hyderabad+500+034&hq=Elahe,+Road+No+10,+Zehra+Nagar,+Banjara+Hills,+Hyderabad,+Andhra+Pradesh+500034,+India&t=m&z=17" target="_blank">see map</a> </p>
+</div>
+<h3>KHIMSAR</h3>
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+	at Khimsar Fort<br />
+	Post Office Khimsar<br />
+	Dist. Nagar-341025<br />
+	Rajasthan<br />
+	Tel.: +91-1585-262345<br /><br />
+<a href="https://maps.google.com/maps?q=Khimsar+Fort,+Post+Office+Khismar,+Nagar-341025&hl=en&ll=27.002243,73.476219&spn=0.275612,0.408897&sll=26.88052,75.774102&sspn=0.137955,0.204449&hq=Khimsar+Fort,+Post+Office&hnear=Khimsar,+Nagaur,+Rajasthan,+India&t=m&z=12&iwloc=B" target="_blank">see map</a> </p>
+</div>
+<h3>UDAIPUR</h3>
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+    at Aashka<br />
+	The Arcade, Manek Chwok<br />
+	The City Palace Complex<br />
+	Udaipur 313001<br />
+	Tel.: +91-294-2423485/86<br /><br />
+		
+<a href="https://maps.google.com/maps?q=manek+chwok,+Udaipur+313001&hl=en&sll=24.576442,73.683511&sspn=0.140655,0.204449&hq=manek+chwok,&hnear=Udaipur,+Rajasthan,+India&t=m&z=16" target="_blank">see map</a> </p>
+
+</div>
+<h3>PUNE</h3>
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+	at The O Hotel<br />
+	Lane No. 5, North Main Road<br />
+	Koregaon Park,<br />
+	Pune 411001<br />
+	Tel.: +91-20-40011000<br /><br />
+<a href="https://maps.google.com/maps?q=o+hotel,+North+Main+Road,+Meera+Nagar,+Koregaon+Park,+Pune,+Maharashtra+411001,+India&hl=en&ll=18.539431,73.893485&spn=0.01833,0.025556&sll=18.53906,73.895116&sspn=0.004583,0.006389&hq=o+hotel,+North+Main+Road,&hnear=Meera+Nagar,+Koregaon+Park,+Pune,+Maharashtra,+India&t=m&z=16&iwloc=A" target="_blank">see map</a> </p>
+</div>
+<h3>KOLKATA</h3>
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+	Forum Courtyard, 1st floor,<br/>
+	10/3, Elgin Road,<br/>
+	Kolkata 700020<br/>
+	Tel: +91-33-40235011<br /><br />
+<a href="https://maps.google.com/maps?q=+Elgin+Road,++Kolkata,+West+Bengal+700020,+India&hl=en&sll=22.553488,88.352062&sspn=0.03571,0.051112&hnear=Lala+Lajpat+Rai+Sarani,+Bhawanipur,+Kolkata,+West+Bengal+700020,+India&t=m&z=17" target="_blank">see map</a> </p>
+</div>
+
+
+</div>
 
 <?php footer(); ?>
