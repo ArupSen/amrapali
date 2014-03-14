f914e5ffbeeda3194fac9805fab6ddd73ab7149a
Arup Sen <arupsen48@gmail.com>
2014-03-14 15:46:26 +0000

    updated all text

--- manufacturing.php
+++ manufacturing.php
@@ -6,22 +6,12 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
-<!-- <div class="logo"></div> -->
 <?php img_tag('amrapali_design_studio.jpg','','collections-logo'); ?>
-<!-- <div class="sub-nav">
-	<a href="" onclick="return false;">FACTORY</a>
-	<a href="http://martnerandmowat.com/amrapali/contact.php">GENERAL</a>
-	<a href="http://martnerandmowat.com/amrapali/contact/careers.php">CAREERS</a>
-</div> -->
 
 <div class="contact_form"><h3>Amrapali Manufacturing Units</h3>
 <hr />
-<p class="contact_info">Amrapali Design Studio & Amrapali Exports cater to domestic and international markets respectively. Manufacturing jewellery for leading international stores and designers, the workshops have installed all productions in house as per international standards.<br /><br />
-Based in the city of Jaipur, Amrapali offers a wide range of bespoke jewellery as per buyers specification in precious and non-precious metals. With the capacity of more than a thousand workers, our manufacturing units are sophisticated with modern machinery, well-trained personnel, and absolute dedication to perfection.</p>
+<p class="contact_info">Amrapali Design Studio & Amrapali Exports cater to domestic and international markets respectively. Manufacturing handcrafted jewellery for leading international stores and designers, the workshops have installed all productions in house as per international standards. Our company respects international social compliance and environmental principles aimed at promoting and protecting human rights and the environment.<br /><br />
+Based in the city of Jaipur, Amrapali offers a wide range of bespoke jewellery as per buyers' specification in precious and non-precious metals. With the capacity of more than a thousand craftsmen, our manufacturing units are equipped with modern machinery, well-trained personnel, and absolute dedication to perfection. For further information please email us on <a href="mailto:amrapali@amrapaliexports.com">amrapali@amrapaliexports.com</a></p>
 </div>
-
-<?php 
-// filename, title, id
-// img_tag('','',''); ?>
 
 <?php footer(); ?>
