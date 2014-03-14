8d7e8d43208f4ecd5b594b456a6c5ffc60946e20
Arup Sen <arupsen48@gmail.com>
2014-03-14 15:05:50 +0000

    updated harrods ext num

---
+++ international.php
@@ -0,0 +1,111 @@
+<?php
+// -- international.php --
+
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+<?php img_tag('Stores.jpg', '', 'stores-logo'); ?>
+
+<?php //img_tag('stores-international.png', '', 'stores-international'); ?>
+<div id="store_locations">
+<h3>UNITED KINGDOM</h3>
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+	55, Beauchamp Place, Knightsbridge,<br />
+	London SW3 1NY, UK<br />
+	Tel.: +44 207584 4433<br />
+	<a href="mailto:london@amrapalijewels.com">london@amrapalijewels.com</a><br /><br />
+	<a href="https://maps.google.com/maps?q=55,+Beauchamp+Place,+Knightsbridge&hl=en&sll=22.537841,88.350236&sspn=0.008928,0.012778&hq=55,+Beauchamp+Place,+Knightsbridge&radius=15000&t=m&z=13&iwloc=F" target="_blank">see map</a>
+</p>
+<p>
+	Amrapali<br />
+	Harrods<br />
+	Fine jewellery room<br />
+
+	87–135 Brompton Road<br />
+
+	Knightsbridge, London SW1X 7XL<br />
+
+	Phone: +44 2077301234 ext 3194<br />
+	<a href="mailto:harrods@amrapalijewels.com">harrods@amrapalijewels.com</a><br /><br />
+	<a href="https://maps.google.com/maps?q=87-135+Brompton+Road,+London+SW1X+7XL,+United+Kingdom&hl=en&ie=UTF8&ll=51.499567,-0.163679&spn=0.012035,0.025556&sll=51.502274,-0.157587&sspn=0.012035,0.025556&oq=87%E2%80%93135+Brompton+RoadKnightsbridge,+London+SW1X+7XL,+United+Kingdom&hnear=87-135+Brompton+Rd,+London+SW1X+7XL,+United+Kingdom&t=m&z=16&iwloc=lyrftr:m,1580309428002753858,51.499406,-0.16325" target="_blank">see map</a>
+</p>
+<p>
+	Amrapali<br />
+	For Dinny Hall,<br />
+	at Liberty Department Store,<br />
+	Regent Street,<br />
+	London W1B 5AH<br />
+	Tel : +44 2077341234<br /><br />
+	<a href="https://maps.google.com/maps?q=Liberty+-+London,+Regent+Street,+London+W1B+5AH,+United+Kingdom&hl=en&sll=51.513787,-0.139958&sspn=0.003008,0.006389&oq=liberLondon+W1B+5AH,+United+Kingdom&hq=Liberty+-+London,+Regent+Street,+London+W1B+5AH,+United+Kingdom&t=m&z=16&iwloc=A" target="_blank">see map</a>
+</p>
+</div>
+<h3>USA</h3>
+<div>
+	<hr />
+<p>
+	  Amrapali INC<br />	
+	  2 West, 46th Street, #905 <br/>
+	  New York, N.Y 10036 <br />
+	  Tel:  212-944-0007<br />	  
+	  <a href="mailto:newyork@amrapalijewels.com">newyork@amrapalijewels.com</a><br /><br />
+	<a href="https://maps.google.com/maps?q=Amrapali+Inc,+West+46th+Street,+New+York+City,+NY+10036,+United+States&hl=en&sll=40.756181,-73.979702&sspn=0.001831,0.003195&oq=amr2+West+46th+Street,+New+York+City,+NY+10036,+United+States&hq=Amrapali+Inc,&hnear=W+46th+St,+New+York,+10036&t=m&z=16" target="_blank">see map</a>
+</p>
+</div>
+<h3>SRI LANKA</h3>
+<div>
+	<hr />
+<p>
+  	Amrapali<br />
+  	TRUNK, The Shopping Piazza<br />
+	The Galle Face Hotel<br /> 
+	Colombo 03<br />
+	Tel.: +94 112 437372<br /><br />
+	<a href="https://maps.google.com/maps?q=TRUNK,+The+Shopping+Piazza.+The+Galle+Face+Hotel.+Colombo+03&hl=en&sll=40.756188,-73.979588&sspn=0.014645,0.025556&hq=TRUNK,+The+Shopping+Piazza.+The+Galle+Face+Hotel.+Colombo+03&radius=15000&t=m&z=14&iwloc=A" target="_blank">see map</a>
+</p>
+</div>
+<h3>SPAIN</h3>
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+	at Sparkles<br />
+	Ramon y Cajal 7<br />
+	Marbella, Spain 29600<br />
+	Tel.: +34 952775659<br /><br />
+	<a href="https://maps.google.com/maps?q=sparkles,+Ramon+y+Cajal+,Marbella,+Spain+29600&hl=en&ll=36.508989,-4.885424&spn=0.001942,0.003195&sll=36.508986,-4.885424&sspn=0.001951,0.003195&t=m&z=19" target="_blank">see map</a>
+</p>
+</div>
+<h3>NIGERIA</h3>
+<div>
+	<hr />
+<p>
+	Amrapali<br />
+	At Temple Muse 8,<br />
+	Sanusi Fafunwa Street<br />
+	Victoria Island, Lagos<br />
+	Tel.: +234 17357842<br /><br />
+	<a href="https://maps.google.com/maps?q=Sanusi+Fafunwa+Street,%09Victoria+Island,+Lagos&hl=en&ll=6.431032,3.427268&spn=0.002401,0.003195&sll=36.508989,-4.885424&sspn=0.001942,0.003195&hnear=Sanusi+Fafunwa+St,+Lekki,+Lagos,+Nigeria&t=m&z=19" target="_blank">see map</a>
+</p>
+</div>
+<h3>CHINA</h3>
+<div>
+	<hr />
+<p>
+	Asulikeit<br />
+	SA-09 Solana, No.6 Chaoyang Park Road,<br />
+	Chaoyang District,<br />
+	Beijing, China, 100026<br />
+	Tel/Fax: +86 10 5905 1759<br/>
+	<a href ="mailto:ddtom911@gmail.com">ddtom911@gmail.com</a><br /><br />
+	<a href="https://maps.google.com/maps?q=SA-09+Solana,No.6+Chaoyang+Park+Road,Chaoyang+District,Beijing,+China,+100026&hl=en&ll=39.924095,116.480253&spn=0.001853,0.003195&sll=39.923971,116.480771&sspn=0.007413,0.012778&t=m&z=19" target="_blank">see map</a>
+</p>
+</div>
+</div>
+
+<?php footer(); ?>
