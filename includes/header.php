c79ae3255ada70245249e146d1158ff164a9f022
Arup Sen <arupsen48@gmail.com>
2014-03-16 23:26:40 +0000

    updated doctype to html5 and removed html tag attributes

--- includes/header.php
+++ includes/header.php
@@ -29,10 +29,8 @@ if (is_nested($nested, $filename)) {
 require_once INCPATH.'functions.php';
 include_once DATAPATH.'meta_tags.php';
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="en">
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<?php // leave in during development and remove once live ?>
