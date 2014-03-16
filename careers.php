96303309fa0fbb82618a7e300a34d3680c6c684d
Arup Sen <arupsen48@gmail.com>
2014-03-16 22:50:53 +0000

    removed unused old sub-nav

--- careers.php
+++ careers.php
@@ -6,23 +6,10 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
-<!-- <div class="logo"></div> -->
 <?php img_tag('careers.jpg','','collections-logo'); ?>
-
-<div class="sub-nav">
-	<a href="" onclick="return false;">FACTORY</a>
-	<a href="http://martnerandmowat.com/amrapali/contact.php">GENERAL</a>
-	<a href="http://martnerandmowat.com/amrapali/contact/careers.php">CAREERS</a>
-</div>
-
 <div class="contact_form"><h3>Join Us</h3>
 <hr />
 <p class="contact_info">At Amrapali there are several different types of job opportunities from corporate opportunities to being a sales associate in one of our stores. We are in search of bright individuals to represent us.<br /><br />
 To inquire about positions, please email us your resum&eacute; (with portfolio mandatory for designers) at jobs@amrapalijewels.com</p>
 </div>
-
-<?php 
-// filename, title, id
-// img_tag('','',''); ?>
-
 <?php footer(); ?>
