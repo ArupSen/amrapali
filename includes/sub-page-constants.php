6a4364702336046770147f02876767bf245f77fd
Arup Sen <arupsen48@gmail.com>
2014-03-16 23:53:14 +0000

    first commit

---
+++ includes/sub-page-constants.php
@@ -0,0 +1,10 @@
+<?php
+// a few file paths for one folder deep
+// -- sub-page-constants.php --
+define('IMGPATH', '../images/');
+define('DATAPATH', '../data/');
+define('CSSPATH', '../css/');
+define('JSPATH', '../js/');
+
+
+?>
