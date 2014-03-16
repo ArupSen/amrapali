f9800c3ee5686935351b62c0a806ddecf8e2a2d6
Arup Sen <arupsen48@gmail.com>
2014-03-16 23:12:23 +0000

    removed redundant lines

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,32 @@
 <?php
+// get the filename
+$filename = basename($_SERVER['SCRIPT_FILENAME'],".php");
+
+// is the file nested
+$nested = array(
+	
+	'',
+	''
+);
+function is_nested($nested, $filename) {
+	$array_count = count($nested);
+	$match_count = 0;
+	for ($i=0; $i < $array_count; $i++) { 
+		if ($filename == $nested[$i]) {
+			$match_count++;
+		}
+	}
+	return $match_count > 0;
+}
+
 // included header file which also inserts meta tags
 // the functions and constants files are also required
-require_once INCPATH.'constants.php';
+if (is_nested($nested, $filename)) {
+	require_once INCPATH.'sub-page-constants.php';
+} else {
+	require_once INCPATH.'constants.php';
+}
 require_once INCPATH.'functions.php';
-
-$filename = basename($_SERVER['SCRIPT_FILENAME'],".php");
 include_once DATAPATH.'meta_tags.php';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -16,7 +38,8 @@ include_once DATAPATH.'meta_tags.php';
 	<?php // leave in during development and remove once live ?>
 	<meta name="robots" content="noindex, nofollow">
 	
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	
 	<meta name="description" content="<?php echo ${$filename}['description']; ?>" />
 	<meta name="keywords" content="<?php echo ${$filename}['keywords']; ?>" />
@@ -24,6 +47,8 @@ include_once DATAPATH.'meta_tags.php';
 	<title><?php echo ${$filename}['title']; ?></title>
 	<link href='http://fonts.googleapis.com/css?family=Quattrocento+Sans' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="<?php echo CSSPATH.'style.css'; ?>" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php echo CSSPATH.'press.css'; ?>" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php echo CSSPATH.'colorbox.css'; ?>" type="text/css" media="screen" />
 	<link rel="shortcut icon" href="<?php echo IMGPATH.'favicon.ico'; ?>" type="image/x-icon" />
 	<!--[if lt IE 7]>
 	<link rel="stylesheet" href="<?php echo CSSPATH.'style_ie.css'; ?>" type="text/css" media="screen" />	
@@ -37,5 +62,5 @@ include_once DATAPATH.'meta_tags.php';
 	<div id="wrap">
 		<div id="heading">
 			<h1 id="mainheading">Amrapali</h1>
-			<h2 id="subheading">JAIPUR</h2>
+			<h2 id="subheading">JAIPUR</h2>			
 		</div>
