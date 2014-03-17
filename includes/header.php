e1706c1515a013689fb1c097590c7622cd875e34
Arup Sen <arupsen48@gmail.com>
2014-03-17 10:42:41 +0000

    updated viewport initial scale=1

--- includes/header.php
+++ includes/header.php
@@ -36,7 +36,7 @@ include_once DATAPATH.'meta_tags.php';
 	<?php // leave in during development and remove once live ?>
 	<meta name="robots" content="noindex, nofollow">
 	
-  <meta name="viewport" content="width=device-width">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	
 	<meta name="description" content="<?php echo ${$filename}['description']; ?>" />
