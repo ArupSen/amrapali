c952fec29ab5327e1c96cd9db03eb73b94034f4a
Arup Sen <arup@arupdesigns.co.uk>
2014-03-26 17:04:27 +0000

    changed email into mailto: hyperlink

--- careers.php
+++ careers.php
@@ -10,6 +10,6 @@ navbar();
 <div class="contact_form"><h3>Join Us</h3>
 <hr />
 <p class="contact_info">At Amrapali there are several different types of job opportunities from corporate opportunities to being a sales associate in one of our stores. We are in search of bright individuals to represent us.<br /><br />
-To inquire about positions, please email us your resum&eacute; (with portfolio mandatory for designers) at jobs@amrapalijewels.com</p>
+To inquire about positions, please email us your resum&eacute; (with portfolio mandatory for designers) at <a href="mailto:jobs@amrapalijewels.com">jobs@amrapalijewels.com</a></p>
 </div>
 <?php footer(); ?>
